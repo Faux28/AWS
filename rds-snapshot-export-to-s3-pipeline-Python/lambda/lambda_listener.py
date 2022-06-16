72a8fc936241c3fa6820f459d52ab464824fb8b7
Mohammed Irfan <95955810+Faux28@users.noreply.github.com>
2022-06-16 15:24:37 +0530

    CDK

--- rds-snapshot-export-to-s3-pipeline-Python/lambda/lambda_listener.py
+++ rds-snapshot-export-to-s3-pipeline-Python/lambda/lambda_listener.py
@@ -9,38 +9,10 @@ s3 = boto3.client('s3')
 logger = logging.getLogger()
 logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
 
-def copy_func(event, context):
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
-    object_key = event['Records'][0]['s3']['object']['key']
-    target_bucket = 'ffi-test'
-    copy_source = {'Bucket': source_bucket, 'Key': object_key}
-    print ("Source bucket : ", source_bucket)
-    print ("Target bucket : ", target_bucket)
-    print ("Log Stream name: ", context.log_stream_name)
-    print ("Mem. limits(MB): ", context.memory_limit_in_mb)
-    try:
-        s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
-        print("object created successfully")
-    except Exception as err:
-        print ("Error -"+str(err))
 
 
 def main(event, context):
 
-    # print(event)
-
-    # for e in event['Records']:
-
-    #     if bool(re.search("ObjectCreated",e['eventName'] )):
-    #         copy_func(event,context)
-
-    #     elif bool(re.search("ObjectRemoved",e['eventName'] )):
-    #         print ("object deleted successfully")
-        
-    # return {
-    #     'statusCode': 200,
-    #     'body': event
-    # }
     if event["Records"][0]["EventSource"] != "aws:sns":
         logger.warning(
             "This function only supports invocations via SNS events, "
--- rds-snapshot-export-to-s3-pipeline-Python/rds_snapshot_export_to_s3_pipeline/rds_snapshot_export_to_s3_pipeline_stack.py
+++ rds-snapshot-export-to-s3-pipeline-Python/rds_snapshot_export_to_s3_pipeline/rds_snapshot_export_to_s3_pipeline_stack.py
@@ -1,26 +1,23 @@
 from typing_extensions import Self
 from aws_cdk import (
     Stack,
-    aws_sqs as sqs,
-    aws_kms as kms,
     aws_glue as glue,
     aws_iam as iam,
     aws_lambda as awslambda,
-    aws_lambda_event_sources as lambda_event_sources,
+    aws_sns_subscriptions as sns_subscription,
     aws_rds as rds,
     aws_s3 as s3,
     aws_sns as sns
 )
-import path as path
 from constructs import Construct
-from enum import Enum
+
 
 # def RdsEventId():
 #     DB_AUTOMATED_SNAPSHOT_CREATED = "RDS-EVENT-0091"
 #     return DB_AUTOMATED_SNAPSHOT_CREATED
 
 
-props={"dbName":"","rdsEventId":"RDS-EVENT-0091"}
+props={"dbName":"database-1","rdsEventId":"RDS-EVENT-0091"}
 class RdsSnapshotExportToS3PipelineStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str,**kwargs) -> None:
@@ -29,7 +26,7 @@ class RdsSnapshotExportToS3PipelineStack(Stack):
         snapshotbucket = s3.Bucket(self, "Bucket",bucket_name="ffi-s3-lambda-rds-pipeline-bucket")
 
         snapshotExportTaskRole = iam.Role(self, "snapshotExportTaskRole",
-                            role_name="snapshotExportTaskRole",
+                            
                             assumed_by=iam.ServicePrincipal("export.rds.amazonaws.com"),
                             description="Role used by RDS to perform snapshot exports to S3")
 
@@ -43,33 +40,33 @@ class RdsSnapshotExportToS3PipelineStack(Stack):
                                     resources=[snapshotbucket.bucket_arn,snapshotbucket.bucket_arn+"/*"])
                                     ]))
 
-        lambdaExecutionRole = iam.Role(self, "lambdaExecutionRole",
-                            role_name="RdsSnapshotExporterLambdaExecutionRole ",
+        lamdarole = iam.Role(self, "lamdarole",
+                            
                             description="RdsSnapshotExportToS3 Lambda execution role for the database.",
                             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"))
 
-        lambdaExecutionRole.attach_inline_policy(iam.Policy(self,"lambdapolicy",policy_name="lambdaExecutionpolicy",statements=[
+        lamdarole.attach_inline_policy(iam.Policy(self,"lambdapolicy",policy_name="lambdaExecutionpolicy",statements=[
                                 iam.PolicyStatement(actions=["rds:StartExportTask"],resources=["*"]),
                                 iam.PolicyStatement(actions=["iam:PassRole"],resources=[snapshotExportTaskRole.role_arn])
                                 ]))
 
-        lambdaExecutionRole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"))
+        lamdarole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"))
 
-        snapshotExportGlueCrawlerRole = iam.Role(self, "snapshotExportGlueCrawlerRole",
-                            role_name="snapshotExportGlueCrawlerRole ",
+        gluecrawlerrole = iam.Role(self, "snapshot_export_gluecrawler_role",
+                            
                             assumed_by=iam.ServicePrincipal("glue.amazonaws.com"))
 
-        snapshotExportGlueCrawlerRole.attach_inline_policy(iam.Policy(self,"gluepolicy",policy_name="snapshotExportGlueCrawlerpolicy",statements=[
+        gluecrawlerrole.attach_inline_policy(iam.Policy(self,"gluepolicy",policy_name="snapshotExportGlueCrawlerpolicy",statements=[
                                 iam.PolicyStatement(actions=["s3:GetObject","s3:PutObject"],resources=[snapshotbucket.bucket_arn+"/*"])
                                 ]))
 
-        snapshotExportGlueCrawlerRole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole"))
+        gluecrawlerrole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole"))
 
         snapshotEventTopic= sns.Topic(self,"SnapshotEventTopic",topic_name="SnapshotEventTopic")
 
-        RdsSnapshotEventNotification = rds.CfnEventSubscription(self,"RdsSnapshotEventNotification",sns_topic_arn=snapshotEventTopic.topic_arn,enabled=True,event_categories=["creation"],source_type="db-snapshot")
+        rds.CfnEventSubscription(self,"RdsSnapshotEventNotification",sns_topic_arn=snapshotEventTopic.topic_arn,enabled=True,event_categories=["creation"],source_type="db-snapshot")
 
-        function = awslambda.Function(self, "Lambda",
+        lambdafunction = awslambda.Function(self, "Lambda",
                                     function_name="ffi-s3-lambda-rds-pipeline-function",
                                     runtime=awslambda.Runtime.PYTHON_3_9,
                                     handler="lambda_listener.main",
@@ -80,21 +77,23 @@ class RdsSnapshotExportToS3PipelineStack(Stack):
         "LOG_LEVEL": "INFO",
         "SNAPSHOT_BUCKET_NAME": snapshotbucket.bucket_name,
         "SNAPSHOT_TASK_ROLE": snapshotExportTaskRole.role_arn,
-        # "SNAPSHOT_TASK_KEY": snapshotExportEncryptionKey.keyArn,
         "DB_SNAPSHOT_TYPE": "snapshot",
          },
-        role=lambdaExecutionRole)
-
-        event_source=lambda_event_sources.SqsEventSource(snapshotEventTopic)
-
-        function.add_event_source(event_source)
-
-        event_source_id = event_source.event_source_mapping_id
+        role=lamdarole)
 
 
-        SnapshotExportCrawler=glue.CfnCrawler(self,"SnapshotExportCrawler",name=props['dbName']+"-rds-snapshot-crawler",
-        role=snapshotExportGlueCrawlerRole.role_arn,targets={"s3Targets":{"path":snapshotbucket.bucket_name}},
-            database_name=props['dbName'],schema_change_policy=glue.CfnCrawler.SchemaChangePolicyProperty(
-        delete_behavior="deleteBehavior"))
+        snapshotEventTopic.add_subscription(sns_subscription.LambdaSubscription(lambdafunction))
+
+
+        SnapshotExportCrawler=glue.CfnCrawler(self,"SnapshotExportCrawler",
+            name=props['dbName']+"-rds-snapshot-crawler",
+            role=gluecrawlerrole.role_arn,
+            
+            targets=glue.CfnCrawler.TargetsProperty(s3_targets=[glue.CfnCrawler.S3TargetProperty(
+            path=snapshotbucket.bucket_name,)]),
+            database_name=props['dbName']
+            )
+        
+        # SnapshotExportCrawler.schema_change_policy.delete_behavior
 
      
